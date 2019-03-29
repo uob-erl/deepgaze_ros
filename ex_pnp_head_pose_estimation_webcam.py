d599600721822c8e10a033876d725ff3e65018b3
Giannis Petousakis <gspetou@gmail.com>
2019-03-29 15:32:57 +0000

    Added path for alienware installation of ROS package

--- ex_pnp_head_pose_estimation_webcam.py
+++ ex_pnp_head_pose_estimation_webcam.py
@@ -127,9 +127,14 @@ def main():
     #my_detector = faceLandmarkDetection('/home/petousakis/deepgaze/etc/shape_predictor_68_face_landmarks.dat')
 
     # Declaring the two classifiers
-    my_cascade = haarCascade("../etc/xml/haarcascade_frontalface_alt.xml", "../etc/xml/haarcascade_profileface.xml")
+    my_cascade = haarCascade("/home/giannis/deepgaze/etc/xml/haarcascade_frontalface_alt.xml", "/home/giannis/deepgaze/etc/xml/haarcascade_profileface.xml")
     # TODO If missing, example file can be retrieved from http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-    my_detector = faceLandmarkDetection('../etc/shape_predictor_68_face_landmarks.dat')
+    my_detector = faceLandmarkDetection('/home/giannis/deepgaze/etc/shape_predictor_68_face_landmarks.dat')
+
+    # Declaring the two classifiers
+    #my_cascade = haarCascade("../etc/xml/haarcascade_frontalface_alt.xml", "../etc/xml/haarcascade_profileface.xml")
+    # TODO If missing, example file can be retrieved from http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+    #my_detector = faceLandmarkDetection('../etc/shape_predictor_68_face_landmarks.dat')
 
     # Declaring the two classifiers
     #my_cascade = haarCascade("./etc/xml/haarcascade_frontalface_alt.xml", "./etc/xml/haarcascade_profileface.xml")
