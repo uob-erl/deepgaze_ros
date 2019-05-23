9cdd7b24184f8584bae125caea459bcba0b65a1f
Giannis Petousakis <gspetou@gmail.com>
2019-05-23 15:19:30 +0100

    lib paths for petou

--- head_orientation/src/head_orientation_node.py
+++ head_orientation/src/head_orientation_node.py
@@ -138,9 +138,9 @@ def main():
 
 
         # Declaring the two classifiers, put you deepgaze_ros path
-        my_cascade = haarCascade("/home/manolis/deepgaze/etc/xml/haarcascade_frontalface_alt.xml", "'/home/manolis/deepgaze/etc/xml/haarcascade_profileface.xml")
+        my_cascade = haarCascade("/home/petousakis/deepgaze/etc/xml/haarcascade_frontalface_alt.xml", "/home/petousakis/deepgaze/etc/xml/haarcascade_profileface.xml")
         # TODO If missing, example file can be retrieved from http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-        my_detector = faceLandmarkDetection('/hri_ws/src/deepgaze_ros/shape_predictor_68_face_landmarks.dat')
+        my_detector = faceLandmarkDetection('/home/petousakis/deepgaze/etc/shape_predictor_68_face_landmarks.dat')
 
 
         no_face_counter = 0
