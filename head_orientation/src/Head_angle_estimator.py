eae9f35c8029b860332e24925e810aef8f042b81
GiannisPet <37540323+GiannisPet@users.noreply.github.com>
2021-09-17 19:03:41 +0100

    Update Head_angle_estimator.py

--- head_orientation/src/Head_angle_estimator.py
+++ head_orientation/src/Head_angle_estimator.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
-# The MIT License (MIT)
-# Copyright (c) 2020 Massimiliano Patacchiola
+##
+# Extended and Customized by Giannis(Ioannis) Petousakis, Extreme Robotics Lab, University of Birmingham 2021
+# Massimiliano Patacchiola 2020
 #
+# The MIT License (MIT)
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
 # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-# Tested on Ubuntu 18.04.3 LTS, OpenCV 4.1.2, Python 3
-# Comparison of 4 different methods on corner detection.
-# Parameters set such that all the methods will find around 500 keypoints in the video.
-# You need a video named "video.mp4" in your script folder for running the code.
-# Videos sourced from: https://www.pexels.com/videos
 
 import rospy
 import cv2
